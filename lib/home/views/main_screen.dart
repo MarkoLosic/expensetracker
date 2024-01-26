7650ac7508a5ab0d4aded17240ac24633107a3f2
marko <markolosic1993@gmail.com>
2024-01-26 10:25:49 +0100

    Added a first card on the app

--- lib/home/views/main_screen.dart
+++ lib/home/views/main_screen.dart
@@ -74,7 +74,7 @@ class MainScreen extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Container(
@@ -89,48 +89,124 @@ class MainScreen extends StatelessWidget {
                     ],
                     transform: const GradientRotation(pi / 4),
                   ),
-                  borderRadius: BorderRadius.circular(25)),
-              child: Column(children: [
-                const Text(
-                  'Totalni balans',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const Text('BAM 2100,00'),
-                Row(
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 4,
+                        color: Colors.grey.shade400,
+                        offset: Offset(5, 5))
+                  ]),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 20,
-                          height: 20,
-                          decoration: const BoxDecoration(
-                              color: Colors.white30, shape: BoxShape.circle),
-                          child: const Center(
-                            child: Icon(
-                              CupertinoIcons.arrow_down,
-                              size: 13,
-                            ),
+                    const Text(
+                      'Totalni balans',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      'BAM 2100,00',
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white30,
+                                    shape: BoxShape.circle),
+                                child: const Center(
+                                  child: Icon(
+                                    CupertinoIcons.arrow_down,
+                                    color: Colors.greenAccent,
+                                    size: 13,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 9,
+                              ),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Prihodi',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                  Text(
+                                    'BAM 3000,00',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              )
+                            ],
                           ),
-                        ),
-                        SizedBox(
-                          width: 9,
-                        ),
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Prihodi'),
-                            Text('BAM 3000,00'),
-                          ],
-                        )
-                      ],
+                          Row(
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white30,
+                                    shape: BoxShape.circle),
+                                child: const Center(
+                                  child: Icon(
+                                    CupertinoIcons.arrow_down,
+                                    color: Colors.greenAccent,
+                                    size: 13,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 9,
+                              ),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Rashodi',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                  Text(
+                                    'BAM 1000,00',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     )
-                  ],
-                )
-              ]),
+                  ]),
             ),
           ],
         ),
