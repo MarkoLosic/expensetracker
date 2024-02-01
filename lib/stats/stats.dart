1a511bd63144254e34ab4f062a5f78107519d18c
markolosic <markolosic1993@gmail.com>
2024-02-01 15:45:57 +0100

    New screen improvments added chart file and data set

--- lib/stats/stats.dart
+++ lib/stats/stats.dart
@@ -1,18 +1,32 @@
+import 'package:expensetracker/stats/chart.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
   const StatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
         child: Column(
           children: [
-            Text(
+            const Text(
               'Transakcije',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(12, 20, 12, 20),
+                child: const MyChart(),
+              ),
             )
           ],
         ),
