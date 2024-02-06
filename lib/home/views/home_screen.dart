231e5fb7ab3060fa126712ebc63efef119742e0c
marko <markolosic1993@gmail.com>
2024-02-06 14:33:33 +0100

    Aded new screen ta add expenses and input fields with button

--- lib/home/views/home_screen.dart
+++ lib/home/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:expensetracker/screens/add_expense/views/add_expense.dart';
 import 'package:expensetracker/stats/stats.dart';
 import 'package:expensetracker/home/views/main_screen.dart';
 import 'package:flutter/cupertino.dart';
@@ -12,55 +13,57 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int index = 0;
-  late Color selectedItem = Colors.blue;
-  Color unselectedItem = Colors.grey;
-
+  var wigetList = const [
+    MainScreen(),
+    StatScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(),
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
-          child: BottomNavigationBar(
-              onTap: (value) {
-                setState(() {
-                  index = value;
-                });
-              },
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              elevation: 3,
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.home,
-                        color: index == 0 ? selectedItem : unselectedItem),
-                    label: 'Home'),
-                BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.graph_square_fill,
-                        color: index == 1 ? selectedItem : unselectedItem),
-                    label: 'Stats'),
-              ]),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            shape: const CircleBorder(),
-            child: Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    colors: [
-                      Theme.of(context).colorScheme.primary,
-                      Theme.of(context).colorScheme.secondary,
-                      Theme.of(context).colorScheme.tertiary,
-                    ],
-                    transform: const GradientRotation(pi / 4),
-                  )),
-              child: const Icon(CupertinoIcons.add),
-            )),
-        body: index == 0 ? const MainScreen() : const StatScreen());
+      // appBar: AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+        child: BottomNavigationBar(
+            onTap: (value) {
+              print(value);
+            },
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 3,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square_fill), label: 'Stats'),
+            ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const AddExpense()),
+            );
+          },
+          shape: const CircleBorder(),
+          child: Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.primary,
+                    Theme.of(context).colorScheme.secondary,
+                    Theme.of(context).colorScheme.tertiary,
+                  ],
+                  transform: const GradientRotation(pi / 4),
+                )),
+            child: const Icon(CupertinoIcons.add),
+          )),
+      body: MainScreen(),
+    );
   }
 }
