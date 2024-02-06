cf6be62ce2fac542ec4da7ecc0aa3a4e3b446af8
marko <markolosic1993@gmail.com>
2024-02-06 15:56:08 +0100

    Adedd new funcionality and controller

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
+
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +33,92 @@ class AddExpense extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
-            TextFormField(),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
-            TextFormField(),
             SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              //This is input field for expenses
+              child: TextFormField(
+                controller: expenseController,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.moneyBills,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'Iznos u BAM',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none)),
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            //This is input field for categories
+            TextFormField(
+              controller: categoryController,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.list,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Kategorija',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none)),
+            ),
+            const SizedBox(
               height: 16,
             ),
-            TextFormField(),
-            SizedBox(
-              height: 16,
+            // This is new input field for the time
+            TextFormField(
+              controller: dateController,
+              textAlignVertical: TextAlignVertical.center,
+              readOnly: true,
+              onTap: () {
+                showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 365)));
+              },
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.clock,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Datum',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none)),
             ),
-            TextButton(onPressed: () {}, child: Text('Sacuvaj'))
+            const SizedBox(
+              height: 32,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: kToolbarHeight,
+              child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  child: const Text(
+                    'Sacuvaj',
+                    style: TextStyle(fontSize: 22, color: Colors.white),
+                  )),
+            )
           ]),
         ),
       ),
